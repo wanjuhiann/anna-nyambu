4dfcc56b7dbaea573471c56cc89de4da34b3364f
wanjuhiann <106369744+wanjuhiann@users.noreply.github.com>
2022-07-14 10:28:05 +0300

    Add files via upload

---
+++ snackbar assignment.c
@@ -0,0 +1,29 @@
+/*
+Zetech c snack bar
+Created by Ann Wanjuhi DIT-01-8862/2021
+On July 9
+C89
+MIT Licence
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    printf("tToday`s menu!\n");
+    printf("1.smokie- ksh 40\n");
+    printf("2.Samosa- ksh 50\n");
+    printf("3.Hamburger- ksh 300\n");
+    printf("4.African tea- ksh 60\n");
+    printf("5.Milkshake-ksh150\n");
+    printf("Enter item no: ");
+    scanf("%d",&item);
+    If(item > 0 && item <=3) (
+       printf("item added to cart\n");
+}
+else (
+     printf("invalid item\n");
+     menu();
+
+     return item
